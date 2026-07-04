58a31b0fed760a21d49b900b35ff9d60fc497a1d
Phoenix89S <134923425+Phoenix89S@users.noreply.github.com>
2026-07-04 17:01:19 +0200

    Update Denis.py

--- Denis.py
+++ Denis.py
@@ -15,6 +15,9 @@ from concurrent.futures import ThreadPoolExecutor
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 # ==========================
 # TURBO CORE
 # ==========================
